bb726a2e1b060a55f78d5efdd8aa2a5eeebd2eb4
Macro Gu <macrogu@qq.com>
2020-05-01 16:42:50 +0800

    添加 直接运行的相关命令

---
+++ run_bin/all_close.bat
@@ -0,0 +1,10 @@
+taskkill /F /IM cwmd.exe
+taskkill /F /IM loginapp.exe
+taskkill /F /IM dbmgr.exe
+taskkill /F /IM timerd.exe
+taskkill /F /IM logapp.exe
+taskkill /F /IM baseapp.exe
+taskkill /F /IM cellapp.exe
+
+
+del /s /Q .\log
